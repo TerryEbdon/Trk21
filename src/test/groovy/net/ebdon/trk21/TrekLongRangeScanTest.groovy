fdb6e6e50a9d2e3eff4a2983c12a36eb342d017a
TerryEbdon <47636739+TerryEbdon@users.noreply.github.com>
2019-04-03 21:22:42 +0100

    Tests are too slow - Closes #21 Isolated the TrekLongRangeScanTest from cooperating classes. Commented-out code that will be moved to a different test class as part of the issue #32 implementation.

--- src/test/groovy/net/ebdon/trk21/TrekLongRangeScanTest.groovy
+++ src/test/groovy/net/ebdon/trk21/TrekLongRangeScanTest.groovy
@@ -1,9 +1,12 @@
 package net.ebdon.trk21;
 
+import static ShipDevice.DeviceType;
+
 import groovy.mock.interceptor.StubFor;
+import groovy.mock.interceptor.MockFor;
 import groovy.transform.TypeChecked;
 
-import static ShipDevice.DeviceType;
+import org.codehaus.groovy.runtime.powerassert.PowerAssertionError;
 
 /**
  * @file
@@ -24,6 +27,7 @@ import static ShipDevice.DeviceType;
  */
 
 @groovy.util.logging.Log4j2('logger')
+@Newify( MockFor )
 final class TrekLongRangeScanTest extends TrekTestBase {
 
   private StubFor shipStub;
@@ -36,40 +40,55 @@ final class TrekLongRangeScanTest extends TrekTestBase {
     trek = new Trek( ui )
   }
 
-  void testLongRangeScanEmptyGalaxy() {
-    logger.info 'testLongRangeScanEmptyGalaxy'
-
-    resetShip 8
-
-    trek.with {
-      shouldFail( org.codehaus.groovy.runtime.powerassert.PowerAssertionError ) {
-        longRangeScan() // Fail: scanning an empty galaxy
-      }
-    }
-    logger.debug ui
-    logger.info 'testLongRangeScanEmptyGalaxy -- OK'
-  }
+  // void testLongRangeScanEmptyGalaxy() {
+  //   logger.info 'testLongRangeScanEmptyGalaxy'
+  //
+  //   resetShip 1
+  //   MockFor galaxyMock = MockFor( Galaxy )        // Pass-through, no demands
+  //   MockFor lrScanMock = MockFor( LongRangeScan )
+  //
+  //   lrScanMock.demand.scanAround { Coords2d quadPos ->
+  //     assert quadPos == shipPos.quadrant
+  //   }
+  //
+  //   galaxyMock.use {
+  //     lrScanMock.use {
+  //       shouldFail( PowerAssertionError ) {
+  //         trek.longRangeScan() // Fail: scanning an empty galaxy
+  //       }
+  //     }
+  //   }
+  //
+  //   assert ui.empty
+  //   shipStub.verify()
+  //   logger.info 'testLongRangeScanEmptyGalaxy -- OK'
+  // }
 
   void testLongRangeScan() {
     logger.info 'testLongRangeScan'
 
     resetShip 10
     scanGoodGalaxy()
-    logger.debug ui
+    assert ui.empty
 
-    assert ui.localMsgLog == ['sensors.longRange.scanQuadrant']
-    assert ui.argsLog == [ [c2d.row, c2d.col] ]
-    assert ui.msgLog == ['  000  000  000', '  000  111  121', '  000  211  221']
+    // logger.debug ui
+    // assert ui.localMsgLog == ['sensors.longRange.scanQuadrant']
+    // assert ui.argsLog == [ [c2d.row, c2d.col] ]
+    // assert ui.msgLog == ['  000  000  000', '  000  111  121', '  000  211  221']
 
     logger.info 'testLongRangeScan -- OK'
   }
 
   private void resetShip( final int expectedPositionCalls ) {
+    MockFor galaxyMock = MockFor( Galaxy )        // Pass-through, no demands
     shipStub = new StubFor( FederationShip )
     shipStub.demand.getPosition( expectedPositionCalls ) { shipPos }
 
-    shipStub.use {
-      trek.ship = new FederationShip()
+    galaxyMock.use {
+      trek.galaxy = new Galaxy()
+      shipStub.use {
+        trek.ship = new FederationShip()
+      }
     }
   }
 
@@ -78,25 +97,36 @@ final class TrekLongRangeScanTest extends TrekTestBase {
     trek.galaxy.board = [ [1,1]: 111, [1,2]: 121, [2,1]: 211, [2,2]: 221 ]
   }
 
-  @TypeChecked
   private void scanGoodGalaxy() {
-    setupGalaxy()
-    trek.longRangeScan()
-  }
-
-  void testScanDamaged() {
-    logger.info 'testScanDamaged'
-    resetShip 10  // Only needed if test fails & scans when it shouldn't.
-    setupGalaxy()  // Only needed if test fails & scans when it shouldn't.
-    trek.with {
-      damageControl.inflictDamage( 3, 9 ) // 9 units of damage to LR sensor.
-      assert damageControl.isDamaged( DeviceType.lrSensor )
-      trek.longRangeScan()
+    // setupGalaxy()
+    MockFor galaxyMock = MockFor( Galaxy )        // Pass-through, no demands
+    MockFor lrScanMock = MockFor( LongRangeScan )
+    lrScanMock.demand.scanAround { Coords2d quadPos ->
+      assert quadPos == shipPos.quadrant
     }
-    logger.debug ui
-    assert ui.msgLog.empty
-    assert ui.localMsgLog == ['sensors.longRange.offline']
+    resetShip 1
 
-    logger.info 'testScanDamaged -- OK'
+    galaxyMock.use {
+      trek.galaxy = new Galaxy()
+      lrScanMock.use {
+        trek.longRangeScan()
+      }
+    }
   }
+
+  // void testScanDamaged() {
+  //   logger.info 'testScanDamaged'
+  //   resetShip 10  // Only needed if test fails & scans when it shouldn't.
+  //   setupGalaxy()  // Only needed if test fails & scans when it shouldn't.
+  //   trek.with {
+  //     damageControl.inflictDamage( 3, 9 ) // 9 units of damage to LR sensor.
+  //     assert damageControl.isDamaged( DeviceType.lrSensor )
+  //     trek.longRangeScan()
+  //   }
+  //   logger.debug ui
+  //   assert ui.msgLog.empty
+  //   assert ui.localMsgLog == ['sensors.longRange.offline']
+  //
+  //   logger.info 'testScanDamaged -- OK'
+  // }
 }
