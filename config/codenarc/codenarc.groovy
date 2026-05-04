0391093da680e1ed523f6cff98a00c7642d0bdd0
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2026-05-04 16:13:25 +0100

    Remove CodeNarc rules that depend on GMetrics.jar Fixes #230

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -376,10 +376,10 @@ ruleset {
     SerializableClassMustDefineSerialVersionUID
 
     // rulesets/size.xml
-    AbcMetric               // Requires the GMetrics jar
+    // AbcMetric               // Requires the GMetrics jar
     ClassSize
-    CrapMetric              // Requires the GMetrics jar and a Cobertura coverage file
-    CyclomaticComplexity    // Requires the GMetrics jar
+    // CrapMetric              // Requires the GMetrics jar and a Cobertura coverage file
+    // CyclomaticComplexity    // Requires the GMetrics jar
     MethodCount
     MethodSize
     NestedBlockDepth
